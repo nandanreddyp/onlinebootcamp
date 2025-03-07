bddb820600346d942e07d5400d1470dce4e143db
nandyiitm <22f3002857@ds.study.iitm.ac.in>
2025-03-07 21:04:12 +0530

    Initial commit

---
+++ routes.py
@@ -0,0 +1,10 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return 'Dashboard'
